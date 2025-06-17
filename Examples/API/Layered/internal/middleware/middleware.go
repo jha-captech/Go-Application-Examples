80f68586e294d4ff41adf910caacd4f90b7ee207
Jonas Ha <jha@captechconsulting.com>
2025-06-16 20:16:55 -0400

    Enhance OpenTelemetry integration by adding middleware support and refactoring logger functionality

--- Examples/API/Layered/internal/middleware/middleware.go
+++ Examples/API/Layered/internal/middleware/middleware.go
@@ -2,22 +2,13 @@ package middleware
 
 import (
 	"net/http"
-	"slices"
+
+	"go.opentelemetry.io/otel"
 )
+
+const name = "example.com/examples/api/layered/internal/middleware"
+
+var tracer = otel.Tracer(name)
 
 // Func is a middleware function that wraps an http.Handler.
 type Func = func(next http.Handler) http.Handler
-
-func WrapHandler(handler http.Handler, middlewares ...Func) http.Handler {
-	if len(middlewares) == 0 {
-		return handler
-	}
-
-	next := handler
-
-	for _, middleware := range slices.Backward(middlewares) {
-		next = middleware(next)
-	}
-
-	return next
-}
--- Examples/API/Layered/internal/telemetry/wrap_handler.go
+++ Examples/API/Layered/internal/telemetry/wrap_handler.go
@@ -3,18 +3,25 @@ package telemetry
 import (
 	"context"
 	"net/http"
+	"slices"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
+
+	"example.com/examples/api/layered/internal/middleware"
 )
 
 // spanKey is a key type used to store the root span in the context.
 type spanKey struct{}
 
+// middlewareFunc is a function type that defines a middleware function.
+type middlewareFunc = func(next http.Handler) http.Handler
+
 // InstrumentedServeMux is a wrapper around http.ServeMux that provides OpenTelemetry instrumentation.
 type InstrumentedServeMux struct {
 	*http.ServeMux
+	middlewares []middleware.Func
 }
 
 // InstrumentServeMux creates a new InstrumentedServeMux that wraps the provided http.ServeMux.
@@ -57,7 +64,12 @@ func (m *InstrumentedServeMux) Handle(pattern string, handler http.Handler) {
 	)
 }
 
-// InstrumentRootHandler wraps the ServeMux's ServeHTTP method with OpenTelemetry instrumentation.
+// InstrumentRootHandler wraps the ServeMux's ServeHTTP method with
+// OpenTelemetry instrumentation and apply the middlewares in order in which it
+// was applied.
+//
+// Note: this method is intended to be the last method called before adding the
+// resulting http.Handler to the http.Server and starting the resulting server.
 func (m *InstrumentedServeMux) InstrumentRootHandler(
 	opts ...otelhttp.Option,
 ) http.Handler {
@@ -65,13 +77,28 @@ func (m *InstrumentedServeMux) InstrumentRootHandler(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
 				ctx := r.Context()
-
 				span := trace.SpanFromContext(ctx)
-
 				ctx = context.WithValue(ctx, spanKey{}, span)
 
-				m.ServeHTTP(w, r.WithContext(ctx))
+				var handler http.Handler = http.HandlerFunc(
+					func(w http.ResponseWriter, r *http.Request) {
+						m.ServeHTTP(w, r)
+					},
+				)
+
+				for _, md := range slices.Backward(m.middlewares) {
+					handler = md(handler)
+				}
+
+				handler.ServeHTTP(w, r.WithContext(ctx))
 			},
 		), "service", opts...,
 	)
+}
+
+// AddMiddleware adds a middleware function to the InstrumentedServeMux.
+// Middleware will be applied in the order they are added, with the last added
+// middleware being applied first.
+func (m *InstrumentedServeMux) AddMiddleware(md middlewareFunc) {
+	m.middlewares = append(m.middlewares, md)
 }
